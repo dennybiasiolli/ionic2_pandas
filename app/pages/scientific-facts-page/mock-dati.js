b6d69db2d3d4c93198ad1c368ddb63c9a75156ff
Denny Biasiolli <denny.biasiolli@gmail.com>
2016-06-09 20:39:18 +0200

    Adding some data

---
+++ app/pages/scientific-facts-page/mock-dati.js
@@ -0,0 +1,601 @@
+export var DATI_PANDAS = {
+    "taglie": [{
+        "nome": "normale",
+        "prezzo": 3
+    }, {
+        "nome": "grosso",
+        "prezzo": 4
+    }, {
+        "nome": "gigante",
+        "prezzo": 5
+    }],
+    "tipologia": [
+        "Qui",
+        "Via"
+    ],
+    "preparazione": [
+        "Caldo",
+        "Freddo"
+    ],
+    "gustiSezioni": [{
+        "id": 1,
+        "nome": "carni e uova",
+        "gusti": [{
+            "id": 1,
+            "nome": "bacon"
+        }, {
+            "id": 2,
+            "nome": "carne impanata"
+        }, {
+            "id": 3,
+            "nome": "wurstel"
+        }, {
+            "id": 4,
+            "nome": "salsiccia"
+        }, {
+            "id": 5,
+            "nome": "uova sode"
+        }, {
+            "id": 6,
+            "nome": "frittate"
+        }]
+    }, {
+        "id": 2,
+        "nome": "salumi",
+        "gusti": [{
+            "id": 7,
+            "nome": "prosciutto cotto"
+        }, {
+            "id": 8,
+            "nome": "prosciutto crudo"
+        }, {
+            "id": 9,
+            "nome": "salame pasta ﬁne"
+        }, {
+            "id": 10,
+            "nome": "salame pasta grossa"
+        }, {
+            "id": 11,
+            "nome": "spianata calabra"
+        }, {
+            "id": 12,
+            "nome": "mortadella"
+        }, {
+            "id": 13,
+            "nome": "speck"
+        }, {
+            "id": 14,
+            "nome": "arrosto di tacchino"
+        }, {
+            "id": 15,
+            "nome": "lardo"
+        }, {
+            "id": 16,
+            "nome": "pancetta arrotolata"
+        }, {
+            "id": 17,
+            "nome": "porchetta"
+        }, {
+            "id": 18,
+            "nome": "bresaola"
+        }]
+    }, {
+        "id": 3,
+        "nome": "formaggi",
+        "gusti": [{
+            "id": 19,
+            "nome": "provola"
+        }, {
+            "id": 20,
+            "nome": "scamorza"
+        }, {
+            "id": 21,
+            "nome": "fontina"
+        }, {
+            "id": 22,
+            "nome": "gorgonzola"
+        }, {
+            "id": 23,
+            "nome": "mozzarella"
+        }, {
+            "id": 24,
+            "nome": "bufala"
+        }, {
+            "id": 25,
+            "nome": "brie"
+        }, {
+            "id": 26,
+            "nome": "stracchino"
+        }, {
+            "id": 27,
+            "nome": "parmigiano"
+        }, {
+            "id": 28,
+            "nome": "pecorino"
+        }, {
+            "id": 29,
+            "nome": "ricotta"
+        }, {
+            "id": 30,
+            "nome": "edamer"
+        }]
+    }, {
+        "id": 4,
+        "nome": "in vasetto",
+        "gusti": [{
+            "id": 31,
+            "nome": "friarielli"
+        }, {
+            "id": 32,
+            "nome": "carcioﬁni"
+        }, {
+            "id": 33,
+            "nome": "cetriolini"
+        }, {
+            "id": 34,
+            "nome": "melanzane"
+        }, {
+            "id": 35,
+            "nome": "funghetti"
+        }, {
+            "id": 36,
+            "nome": "pomodori secchi"
+        }, {
+            "id": 37,
+            "nome": "peperoncini"
+        }, {
+            "id": 38,
+            "nome": "zucchine"
+        }, {
+            "id": 39,
+            "nome": "mais"
+        }]
+    }, {
+        "id": 5,
+        "nome": "verdura cruda",
+        "gusti": [{
+            "id": 40,
+            "nome": "insalata verde"
+        }, {
+            "id": 41,
+            "nome": "insalata rossa"
+        }, {
+            "id": 42,
+            "nome": "rucola"
+        }, {
+            "id": 43,
+            "nome": "pomodoro"
+        }, {
+            "id": 44,
+            "nome": "carote"
+        }, {
+            "id": 45,
+            "nome": "ﬁnocchi"
+        }, {
+            "id": 46,
+            "nome": "funghi"
+        }, {
+            "id": 47,
+            "nome": "cipolla di tropea"
+        }]
+    }, {
+        "id": 6,
+        "nome": "verdura cotta",
+        "gusti": [{
+            "id": 48,
+            "nome": "peperoni grigliati"
+        }, {
+            "id": 49,
+            "nome": "peperoni saltati"
+        }, {
+            "id": 50,
+            "nome": "zucchine grigliate"
+        }, {
+            "id": 51,
+            "nome": "zucchine saltate"
+        }, {
+            "id": 52,
+            "nome": "melanzane grigliate"
+        }, {
+            "id": 53,
+            "nome": "patate bollite"
+        }, {
+            "id": 54,
+            "nome": "patate saltate"
+        }, {
+            "id": 55,
+            "nome": "patate fritte"
+        }, {
+            "id": 56,
+            "nome": "cipolle croccanti"
+        }, {
+            "id": 57,
+            "nome": "cipolle saltate"
+        }, {
+            "id": 58,
+            "nome": "crauti"
+        }, {
+            "id": 59,
+            "nome": "funghi trifolati"
+        }, {
+            "id": 60,
+            "nome": "spinaci al burro"
+        }, {
+            "id": 61,
+            "nome": "friggitelli"
+        }, {
+            "id": 62,
+            "nome": "fagioli"
+        }]
+    }, {
+        "id": 7,
+        "nome": "pesce",
+        "gusti": [{
+            "id": 63,
+            "nome": "acciughe"
+        }, {
+            "id": 64,
+            "nome": "tonno"
+        }, {
+            "id": 65,
+            "nome": "pesce impanato"
+        }]
+    }, {
+        "id": 8,
+        "nome": "specials",
+        "gusti": [{
+            "id": 66,
+            "nome": "Vieni a scoprirlo!"
+        }, {
+            "id": 67,
+            "nome": "Vieni a scoprirlo!"
+        }, {
+            "id": 68,
+            "nome": "Vieni a scoprirlo!"
+        }, {
+            "id": 69,
+            "nome": "Vieni a scoprirlo!"
+        }, {
+            "id": 70,
+            "nome": "Vieni a scoprirlo!"
+        }]
+    }],
+    "sezioni": [{
+        "id": 1,
+        "nome": "carni e uova"
+    }, {
+        "id": 2,
+        "nome": "salumi"
+    }, {
+        "id": 3,
+        "nome": "formaggi"
+    }, {
+        "id": 4,
+        "nome": "in vasetto"
+    }, {
+        "id": 5,
+        "nome": "verdura cruda"
+    }, {
+        "id": 6,
+        "nome": "verdura cotta"
+    }, {
+        "id": 7,
+        "nome": "pesce"
+    }, {
+        "id": 8,
+        "nome": "specials"
+    }],
+    "gusti": [{
+        "idSezione": 1,
+        "id": 1,
+        "nome": "bacon"
+    }, {
+        "idSezione": 1,
+        "id": 2,
+        "nome": "carne impanata"
+    }, {
+        "idSezione": 1,
+        "id": 3,
+        "nome": "wurstel"
+    }, {
+        "idSezione": 1,
+        "id": 4,
+        "nome": "salsiccia"
+    }, {
+        "idSezione": 1,
+        "id": 5,
+        "nome": "uova sode"
+    }, {
+        "idSezione": 1,
+        "id": 6,
+        "nome": "frittate"
+    }, {
+        "idSezione": 2,
+        "id": 7,
+        "nome": "prosciutto cotto"
+    }, {
+        "idSezione": 2,
+        "id": 8,
+        "nome": "prosciutto crudo"
+    }, {
+        "idSezione": 2,
+        "id": 9,
+        "nome": "salame pasta ﬁne"
+    }, {
+        "idSezione": 2,
+        "id": 10,
+        "nome": "salame pasta grossa"
+    }, {
+        "idSezione": 2,
+        "id": 11,
+        "nome": "spianata calabra"
+    }, {
+        "idSezione": 2,
+        "id": 12,
+        "nome": "mortadella"
+    }, {
+        "idSezione": 2,
+        "id": 13,
+        "nome": "speck"
+    }, {
+        "idSezione": 2,
+        "id": 14,
+        "nome": "arrosto di tacchino"
+    }, {
+        "idSezione": 2,
+        "id": 15,
+        "nome": "lardo"
+    }, {
+        "idSezione": 2,
+        "id": 16,
+        "nome": "pancetta arrotolata"
+    }, {
+        "idSezione": 2,
+        "id": 17,
+        "nome": "porchetta"
+    }, {
+        "idSezione": 2,
+        "id": 18,
+        "nome": "bresaola"
+    }, {
+        "idSezione": 3,
+        "id": 19,
+        "nome": "provola"
+    }, {
+        "idSezione": 3,
+        "id": 20,
+        "nome": "scamorza"
+    }, {
+        "idSezione": 3,
+        "id": 21,
+        "nome": "fontina"
+    }, {
+        "idSezione": 3,
+        "id": 22,
+        "nome": "gorgonzola"
+    }, {
+        "idSezione": 3,
+        "id": 23,
+        "nome": "mozzarella"
+    }, {
+        "idSezione": 3,
+        "id": 24,
+        "nome": "bufala"
+    }, {
+        "idSezione": 3,
+        "id": 25,
+        "nome": "brie"
+    }, {
+        "idSezione": 3,
+        "id": 26,
+        "nome": "stracchino"
+    }, {
+        "idSezione": 3,
+        "id": 27,
+        "nome": "parmigiano"
+    }, {
+        "idSezione": 3,
+        "id": 28,
+        "nome": "pecorino"
+    }, {
+        "idSezione": 3,
+        "id": 29,
+        "nome": "ricotta"
+    }, {
+        "idSezione": 3,
+        "id": 30,
+        "nome": "edamer"
+    }, {
+        "idSezione": 4,
+        "id": 31,
+        "nome": "friarielli"
+    }, {
+        "idSezione": 4,
+        "id": 32,
+        "nome": "carcioﬁni"
+    }, {
+        "idSezione": 4,
+        "id": 33,
+        "nome": "cetriolini"
+    }, {
+        "idSezione": 4,
+        "id": 34,
+        "nome": "melanzane"
+    }, {
+        "idSezione": 4,
+        "id": 35,
+        "nome": "funghetti"
+    }, {
+        "idSezione": 4,
+        "id": 36,
+        "nome": "pomodori secchi"
+    }, {
+        "idSezione": 4,
+        "id": 37,
+        "nome": "peperoncini"
+    }, {
+        "idSezione": 4,
+        "id": 38,
+        "nome": "zucchine"
+    }, {
+        "idSezione": 4,
+        "id": 39,
+        "nome": "mais"
+    }, {
+        "idSezione": 5,
+        "id": 40,
+        "nome": "insalata verde"
+    }, {
+        "idSezione": 5,
+        "id": 41,
+        "nome": "insalata rossa"
+    }, {
+        "idSezione": 5,
+        "id": 42,
+        "nome": "rucola"
+    }, {
+        "idSezione": 5,
+        "id": 43,
+        "nome": "pomodoro"
+    }, {
+        "idSezione": 5,
+        "id": 44,
+        "nome": "carote"
+    }, {
+        "idSezione": 5,
+        "id": 45,
+        "nome": "ﬁnocchi"
+    }, {
+        "idSezione": 5,
+        "id": 46,
+        "nome": "funghi"
+    }, {
+        "idSezione": 5,
+        "id": 47,
+        "nome": "cipolla di tropea"
+    }, {
+        "idSezione": 6,
+        "id": 48,
+        "nome": "peperoni grigliati"
+    }, {
+        "idSezione": 6,
+        "id": 49,
+        "nome": "peperoni saltati"
+    }, {
+        "idSezione": 6,
+        "id": 50,
+        "nome": "zucchine grigliate"
+    }, {
+        "idSezione": 6,
+        "id": 51,
+        "nome": "zucchine saltate"
+    }, {
+        "idSezione": 6,
+        "id": 52,
+        "nome": "melanzane grigliate"
+    }, {
+        "idSezione": 6,
+        "id": 53,
+        "nome": "patate bollite"
+    }, {
+        "idSezione": 6,
+        "id": 54,
+        "nome": "patate saltate"
+    }, {
+        "idSezione": 6,
+        "id": 55,
+        "nome": "patate fritte"
+    }, {
+        "idSezione": 6,
+        "id": 56,
+        "nome": "cipolle croccanti"
+    }, {
+        "idSezione": 6,
+        "id": 57,
+        "nome": "cipolle saltate"
+    }, {
+        "idSezione": 6,
+        "id": 58,
+        "nome": "crauti"
+    }, {
+        "idSezione": 6,
+        "id": 59,
+        "nome": "funghi trifolati"
+    }, {
+        "idSezione": 6,
+        "id": 60,
+        "nome": "spinaci al burro"
+    }, {
+        "idSezione": 6,
+        "id": 61,
+        "nome": "friggitelli"
+    }, {
+        "idSezione": 6,
+        "id": 62,
+        "nome": "fagioli"
+    }, {
+        "idSezione": 7,
+        "id": 63,
+        "nome": "acciughe"
+    }, {
+        "idSezione": 7,
+        "id": 64,
+        "nome": "tonno"
+    }, {
+        "idSezione": 7,
+        "id": 65,
+        "nome": "pesce impanato"
+    }, {
+        "idSezione": 8,
+        "id": 66,
+        "nome": "Vieni a scoprirlo!"
+    }, {
+        "idSezione": 8,
+        "id": 67,
+        "nome": "Vieni a scoprirlo!"
+    }, {
+        "idSezione": 8,
+        "id": 68,
+        "nome": "Vieni a scoprirlo!"
+    }, {
+        "idSezione": 8,
+        "id": 69,
+        "nome": "Vieni a scoprirlo!"
+    }, {
+        "idSezione": 8,
+        "id": 70,
+        "nome": "Vieni a scoprirlo!"
+    }],
+
+    "salse": [{
+        "id": 1,
+        "nome": "ketchup"
+    }, {
+        "id": 2,
+        "nome": "maionese"
+    }, {
+        "id": 3,
+        "nome": "barbecue"
+    }, {
+        "id": 4,
+        "nome": "senape"
+    }, {
+        "id": 5,
+        "nome": "salsa rosa"
+    }, {
+        "id": 6,
+        "nome": "tartara"
+    }, {
+        "id": 7,
+        "nome": "yogurt"
+    }, {
+        "id": 8,
+        "nome": "Pan.da's piccante"
+    }, {
+        "id": 9,
+        "nome": "Pan.da's dolce"
+    }, {
+        "id": 10,
+        "nome": "Pan.da's saporita"
+    }]
+};
--- app/pages/scientific-facts-page/scientific-facts-page.js
+++ app/pages/scientific-facts-page/scientific-facts-page.js
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {SelectedFactPage} from '../selected-fact-page/selected-fact-page';
+import {DATI_PANDAS} from './mock-dati';
 
 @Component({
   templateUrl: 'build/pages/scientific-facts-page/scientific-facts-page.html'
@@ -15,16 +16,25 @@ export class ScientificFactsPage {
   }
 
   ionViewWillEnter(){
-    let _facts = [];
-    _facts.push('Banging your head against a wall uses 150 calories an hour.');
-    _facts.push('Dogs have four toes on their hind feet, and five on their front feet.');
-    _facts.push('The ant can lift 50 times its own weight, can pull 30 times its own weight and always falls over on its right side when intoxicated.');
-    _facts.push('A cockroach will live nine days without it\'s head, before it starves to death.');
-    _facts.push('Polar bears are left handed.');
-    this.facts = _facts;
+    // let _facts = [];
+    // _facts.push('Banging your head against a wall uses 150 calories an hour.');
+    // _facts.push('Dogs have four toes on their hind feet, and five on their front feet.');
+    // _facts.push('The ant can lift 50 times its own weight, can pull 30 times its own weight and always falls over on its right side when intoxicated.');
+    // _facts.push('A cockroach will live nine days without it\'s head, before it starves to death.');
+    // _facts.push('Polar bears are left handed.');
+    // this.facts = _facts;
+    
+    this.gustiSezioni = DATI_PANDAS.gustiSezioni;
   }
 
-  selectFact(fact){
-    this._navController.push(SelectedFactPage, {selectedFact: fact})
-  }
+  // selectFact(fact){
+  //   this._navController.push(SelectedFactPage, {selectedFact: fact})
+  // }
+}
+
+export class Gusto {
+    constructor(_numero, _descrizione) {
+        this.numero = _numero;
+        this.descrizione = _descrizione;
+    }
 }
